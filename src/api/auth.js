11705276fca8db7e42205303d67ce427095e8926
Leslie Cheng <leslie.cheng5@gmail.com>
2019-09-14 03:15:45 -0700

    Added some logging to my API calls.

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,6 +2,7 @@ import hash from './helpers/hash.js';
 import gateway from './helpers/gateway.js';
 
 export function handler(event, context, callback) {
+    console.log(event);
     if (gateway(event.body)) {
         callback(null, {
             statusCode: 200,
