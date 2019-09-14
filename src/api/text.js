8803a7359b97fbd1e79b02196080587254be6b4e
Leslie Cheng <leslie.cheng5@gmail.com>
2019-09-14 03:19:15 -0700

    Error handling for bad text requests.

--- src/api/text.js
+++ src/api/text.js
@@ -4,11 +4,18 @@ import sms from './helpers/sms.js';
 export function handler(event, context, callback) {
     console.log(event);
     if (verify(event.headers.authorization)) {
-        sms(event.headers.to, event.body).then(res => {
+        sms(event.headers.to, event.body)
+        .then(res => {
             callback(null, {
                 statusCode: 200,
                 body: res
             });
+        }, 
+        rej => {
+            callback(null, {
+                statusCode: 400,
+                body: rej
+            }
         });
     } else {
         callback(null, {
