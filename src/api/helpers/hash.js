75b520b3093f60497225327519404a3ee4058e05
Leslie Cheng <leslie.cheng5@gmail.com>
2019-09-14 03:12:20 -0700

    Fix ImportError.

--- src/api/helpers/hash.js
+++ src/api/helpers/hash.js
@@ -1,10 +1,11 @@
 import crypto from 'crypto';
+import util from 'util';
 require('dotenv').config();
 
 /**
  * This function returns the hash based on the seed in env.
  */
 export default function hash() {
-    let seed = new TextEncoder('utf-8').encode(process.env.HASH_SEED);
+    let seed = new util.TextEncoder('utf-8').encode(process.env.HASH_SEED);
     return crypto.createHmac('sha256', seed).update('update').digest('hex');
 }
