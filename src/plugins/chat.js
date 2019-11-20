4f9b6bc09a85f50989bb64c0cbd4b2a18b284e6a
Leslie Cheng <leslie.cheng5@gmail.com>
2019-11-20 05:20:59 -0800

    Full duplex group chat impl.

--- src/plugins/chat.js
+++ src/plugins/chat.js
@@ -1,44 +1,136 @@
 import Peer from "peerjs";
+import colors from "../dracula.js";
 
 const chat = {
-  description: "chat with people",
-  help: ["Usage", ""],
+  description: "chat with people, p2p",
+  help: [
+    "Usage",
+    "",
+    "You'll get instructions. Just try it.",
+    "It's peer to peer with webRTC."
+  ],
   async function() {
     let userName = await this.parent.terminal.input("Name: ");
 
-    let id = generateID();
-    let peer = new Peer(id);
+    let help = [
+      'Press "c" to create a new chat room.',
+      'Press "j" to join a chat room.',
+      'Type "/e" to exit.',
+      'Type "/h" for help.'
+    ];
 
-    this.parent.terminal.writeln("Your name is " + userName);
-    this.parent.terminal.writeln("Your peer ID is: " + peer.id);
+    this.parent.terminal.writeln(help);
 
-    let peerId = await this.parent.terminal.input("Peer ID: ");
+    let choosing = true;
 
-    let connection = peer.connect(peerId);
+    /**
+     * peerjs doesn't like bidirectional connections, so
+     * we have to open a connection to each peer that connects.
+     *
+     * We use outgoingPeerIds to track the ones we've connected to.
+     */
+    let peer = null;
+    let peers = [];
+    let outgoingPeerIds = [];
+
+    /**
+     * This function will connect to the peerId if we're not already
+     * connected, and print out a message when we've connected.
+     */
+    let processLogic = (peerId, newUserMessage) => {
+      if (!outgoingPeerIds.includes(peerId)) {
+        let connection = peer.connect(peerId);
+        peers.push(connection);
+        outgoingPeerIds.push(peerId);
+        connection.on("open", () => {
+          this.parent.terminal.writeln(newUserMessage);
+          connection.send(outgoingPeerIds.filter(e => e !== connection.peer));
+        });
+        connection.on("close", () => {
+          this.parent.terminal.writeln("User disconnected.");
+        });
+      }
+    };
+
+    while (choosing) {
+      let choice = await this.parent.terminal.input("Choose: ");
+      let id;
+      switch (choice) {
+        case "c":
+          peer = new Peer(generateID());
+          this.parent.terminal.writeln("Your ID is: " + peer.id);
+          choosing = false;
+          break;
+        case "j":
+          peer = new Peer();
+          id = await this.parent.terminal.input("ID: ");
+          outgoingPeerIds.push(id);
+          peers.push(peer.connect(id));
+          peers[0].on("open", () => {
+            this.parent.terminal.writeln("Connected to other user.");
+          });
+          choosing = false;
+          break;
+        case "/e":
+          return;
+        case "/h":
+          this.parent.terminal.writeln(help);
+          break;
+      }
+    }
+
+    // Remove the connect help messages
+    help.shift();
+    help.shift();
 
     peer.on("connection", conn => {
-      console.log("new connection");
+      /**
+       * When a new connection is opened (when a peer connects to us
+       * directly), this will be called. processLogic will then compare
+       * the peer's ID with the ones we've currently connected to to
+       * determine if we have to open a connection to them too.
+       */
+      conn.on("open", () => {
+        processLogic(conn.peer, "New user joined.");
+      });
+
       conn.on("data", data => {
-        console.log(data);
-        this.parent.terminal.writeln(data);
+        /**
+         * During a handshake, we should receive an array with all
+         * the other peers to this server. We process each peer and
+         * try to connect to them in order to open up a connection.
+         */
+        if (Array.isArray(data)) {
+          data.map(peerId => {
+            processLogic(peerId, "Connected to other user.");
+          });
+        } else {
+          this.parent.terminal.writeln(data);
+        }
       });
     });
 
-    this.parent.terminal.container.addEventListener("keydown", e => {
-      console.log(e);
-    });
-
-    connection.on("open", async () => {
-      while (true) {
-        connection.send(await this.parent.terminal.input("Message: "));
+    while (true) {
+      let msg = await this.parent.terminal.input(
+        `<a style='color:${colors.cyan}'>${userName}:</a> `
+      );
+      switch (msg) {
+        case "/e":
+          // Ensure that we've closed all our connections and peer
+          peers.map(p => p.close());
+          return new Promise(resolve => {
+            peer.on("close", () => {
+              resolve();
+            });
+            peer.destroy();
+          });
+        case "/h":
+          this.parent.terminal.writeln(help);
+          break;
+        default:
+          peers.map(p => p.send(`${userName}: ${msg}`));
       }
-    });
-
-    return new Promise(resolve => {
-      peer.on("error", () => {
-        resolve();
-      });
-    });
+    }
   }
 };
 
@@ -47,7 +139,7 @@ function generateID() {
   let characters =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  for (let i = 0; i < 30; i++) {
+  for (let i = 0; i < 4; i++) {
     id += characters[Math.floor(Math.random() * characters.length)];
   }
 
