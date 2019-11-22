bbdbb5954758a26a1f5895fcc6e8ade49e9c5c63
Leslie Cheng <leslie.cheng5@gmail.com>
2019-11-22 11:30:33 -0800

    Added spongebob command.

---
+++ src/plugins/spongebob.js
@@ -0,0 +1,33 @@
+const spongebob = {
+  description: "spongbob your text",
+  help: [
+    "Usage",
+    "",
+    "spongebob [string]",
+    "",
+    "The string upper/lower permutation is random."
+  ],
+  function(e) {
+    let parsed = e.split(" ");
+
+    parsed.shift();
+
+    let joined = parsed.reduce((prev, curr) => {
+      if (prev !== "") {
+        return prev + " " + curr;
+      } else {
+        return prev + curr;
+      }
+    }, "");
+
+    let ret = "";
+
+    for (let c of joined) {
+      ret += Math.round(Math.random()) ? c.toUpperCase() : c.toLowerCase();
+    }
+
+    this.parent.terminal.writeln(ret);
+  }
+};
+
+export default spongebob;
