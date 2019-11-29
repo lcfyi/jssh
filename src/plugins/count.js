155c521944ac0ac44a16a8de3dbd93a9a22277b8
lcfyi <3242609+lcfyi@users.noreply.github.com>
2019-11-28 17:45:21 -0800

    Added count command.

---
+++ src/plugins/count.js
@@ -0,0 +1,15 @@
+const count = {
+  description: "counts the number of characters in a string",
+  help: [
+    "Usage",
+    "",
+    "count [string]",
+    "Returns the character count of the string."
+  ],
+  function(e) {
+    let expression = e.substring(e.indexOf(" ") + 1);
+    this.parent.terminal.writeln(`Character count: ${expression.length}`);
+  }
+};
+
+export default count;
