c6d2497ef6b30a30955472757d11d68deca9f63c
Leslie Cheng <leslie.cheng5@gmail.com>
2020-03-31 12:06:20 -0700

    index.html as a part of src now.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
@@ -16,9 +18,16 @@ module.exports = {
       cleanOnceBeforeBuildPatterns: ["**/*", "!.gitignore"]
     }),
     new CopyPlugin([{ from: "static", to: "." }]),
+    new HtmlWebpackPlugin({
+      template: "src/index.html"
+    }),
     new WorkboxPlugin.GenerateSW({
       clientsClaim: true,
       skipWaiting: true
     })
-  ]
+  ],
+  output: {
+    filename: "index.[contenthash].js",
+    path: path.resolve(__dirname, "dist")
+  }
 };
