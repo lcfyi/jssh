1c37b426df605103bf415868bbae32dfab6ef187
Leslie Cheng <leslie.cheng5@gmail.com>
2021-05-08 19:20:06 -0700

    Colour update.

--- src/colors.js
+++ src/colors.js
@@ -13,19 +13,34 @@
 //   yellow: "#f1fa8c"
 // };
 
-const onedark = {
-  background: "#282c34",
-  currentLine: "#49515f",
-  selection: "#49515f",
-  foreground: "#abb2bf",
-  comment: "#7f848e",
-  cyan: "#56b6c2",
-  green: "#98c379",
-  orange: "#ffb86c",
-  pink: "#c678dd",
-  purple: "#c678dd",
-  red: "#e06c75",
-  yellow: "#d19a66"
+// const onedark = {
+//   background: "#282c34",
+//   currentLine: "#49515f",
+//   selection: "#49515f",
+//   foreground: "#abb2bf",
+//   comment: "#7f848e",
+//   cyan: "#56b6c2",
+//   green: "#98c379",
+//   orange: "#ffb86c",
+//   pink: "#c678dd",
+//   purple: "#c678dd",
+//   red: "#e06c75",
+//   yellow: "#d19a66",
+// };
+
+const github = {
+  background: "#0d1117",
+  currentLine: "#6e7681",
+  selection: "#6e7681",
+  foreground: "#b1bac4",
+  comment: "#8b949e",
+  cyan: "#76e3ea",
+  green: "#3fb950",
+  orange: "#db6d28",
+  pink: "#d2a8ff",
+  purple: "#bc8cff",
+  red: "#ff7b72",
+  yellow: "#e3b341",
 };
 
-export default onedark;
+export default github;
